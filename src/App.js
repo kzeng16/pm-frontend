ac7868bb63676e0327955cc9b8d640ff324c6951
ZTatman <tatmanzachariahit@gmail.com>
2020-04-10 17:43:15 -0400

    Parking Manager Project Frontend

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+// Components
+import Login from './components/Login';
+
+
+function App() {
+  return (
+    <div className="App">
+    <Login/>
+    </div>
+  );
+}
+
+export default App;
